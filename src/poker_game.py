2aaa879569247a56bb57c8129a26ea2754a7bc70
Vilius Drumsta <vdrumsta@gmail.com>
2020-05-10 00:12:24 +0100

    Add a monte carlo search tree bot

--- src/poker_game.py
+++ src/poker_game.py
@@ -1,9 +1,9 @@
 from pypokerengine.api.game import setup_config, start_poker
-from fish_player import FishPlayer
+from monte_carlo_bot import MonteCarloBot
 
-config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
-config.register_player(name="p1", algorithm=FishPlayer())
-config.register_player(name="p2", algorithm=FishPlayer())
-config.register_player(name="p3", algorithm=FishPlayer())
+config = setup_config(max_round=99, initial_stack=1000, small_blind_amount=10)
+config.register_player(name="p1", algorithm=MonteCarloBot())
+config.register_player(name="p2", algorithm=MonteCarloBot())
+config.register_player(name="p3", algorithm=MonteCarloBot())
 game_result = start_poker(config, verbose=1)
 print(game_result)
